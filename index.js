550061a8902d3c147ec55006a2ac7e84b2a182bf
chibujax <chibujax@gmail.com>
2017-02-14 07:25:30 +0100

    Update index.js with all codes

--- index.js
+++ index.js
@@ -14,3 +14,37 @@
       }
     }
   };
+
+function onErr(err) {
+  console.log(err);
+  return 1;
+}
+
+prompt.start(); 
+prompt.get(schema, function (err, result) {
+  if (err) { return onErr(err); }
+  var client = new Client();
+  var apiurl = "http://services.groupkt.com/country/get/iso2code/" + result.word;
+
+  client.get(apiurl, function (data, response) { 
+    if(!data.hasOwnProperty("RestResponse")){
+      console.log("Something went wrong, contact admin");
+    }
+    else if (!data["RestResponse"].hasOwnProperty("result")) {
+      console.log("No country found");
+    }else{
+      if (data["RestResponse"]["result"].hasOwnProperty("name")){
+        console.log("Country Name : " + data["RestResponse"]["result"]["name"] );
+      }
+      if (data["RestResponse"]["result"].hasOwnProperty("alpha2_code")){
+        console.log("Alpha2_code : " + data["RestResponse"]["result"]["alpha2_code"] );
+      }
+      if (data["RestResponse"]["result"].hasOwnProperty("alpha3_code")) {
+        console.log("Alpha3_code : " + data["RestResponse"]["result"]["alpha3_code"] );
+      }
+    } 
+  }).on('error', function (err) {
+    console.log('something went wrong on the request', err.request.options);
+  });
+
+});
